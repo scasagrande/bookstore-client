f5738923eebb4ba32c663eb2a26300981254f82e
Steven Casagrande <scasagrande@galvant.ca>
2019-03-18 15:25:05 -0400

    [F001] Populate author biography from API

--- app/models/author.js
+++ app/models/author.js
@@ -3,6 +3,7 @@ import Publisher from './publisher';
 import { on } from '@ember/object/evented';
 
 export default Publisher.extend({
+  biography: DS.attr('string'),
   books: DS.hasMany('book'),
 
   loadedAt: on('didLoad', function() {
