fdcc13607db97db2b505ee0f8162e7377010e9e8
Steven Casagrande <scasagrande@galvant.ca>
2019-03-19 14:12:52 -0400

    [F002] a11y and rendering improvements

--- app/components/book-cover.js
+++ app/components/book-cover.js
@@ -1,18 +1,30 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-    actions: {
-
-        open() {
+    keyPress(event) {
+        if ((event.keyCode === 13) || (event.keyCode === 32)) {
+            this.showModal(true);
+        }
+        else if (event.keyCode === 27) {
+            this.showModal(false);
+        }
+    },
+    showModal(showModal) {
+        if (showModal) {
             this.get('book').reload().then(() => {
                 this.set('isShowingModal', true);
                 this.get('blurBackground')(true);
             });
-        },
-
-        close() {
+        }
+        else {
             this.set('isShowingModal', false);
             this.get('blurBackground')(false);
+        }
+    },
+    actions: {
+
+        showModal(state) {
+            this.showModal(state);
         }
     
     }
